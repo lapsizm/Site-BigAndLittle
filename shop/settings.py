f049aeacc9f5b826d77d506242c131f24b4e0c4c
lapsizm <78648610+lapsizm@users.noreply.github.com>
2022-01-08 01:49:03 +0300

    Update settings.py

--- shop/settings.py
+++ shop/settings.py
@@ -139,5 +139,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'big8little587@gmail.com'
-EMAIL_HOST_PASSWORD = 'warface_2002'
+EMAIL_HOST_USER = *********************
+EMAIL_HOST_PASSWORD = *******************
